637dac569d1d0ce5537bc5ebeb484774bcdee28f
Klay Clarke <clarkeklay@gmail.com>
2022-06-17 22:38:58 -0400

    create component for user login form

--- Frontend/src/app/login/login.component.ts
+++ Frontend/src/app/login/login.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+
+import { UserLoginForm } from 'models/userLoginForm';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  form!: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder, private http: HttpClient) {}
 
   ngOnInit(): void {
+    this.form = this.fb.group({
+      email: '',
+      password: '',
+    });
+
+    this.form.valueChanges.subscribe(console.log);
   }
 
+  clearForm() {
+    this.form = this.fb.group({
+      email: '',
+      password: '',
+    });
+  }
+
+  async submitHandler() {
+    const formValue: UserLoginForm = this.form.value;
+    console.warn(formValue);
+    this.clearForm();
+  }
 }
