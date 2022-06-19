4685fe2e71174dd96fad2361193366989947f187
Klay Clarke <clarkeklay@gmail.com>
2022-06-18 23:57:12 -0400

    call backend and render posts -- create method that takes a string with datetime format and returns it as a "fromNow" format (using moment)

--- Frontend/src/app/posts/posts.component.ts
+++ Frontend/src/app/posts/posts.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Post } from 'models/Post';
+import { environment } from 'src/environments/environment';
+
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
-  styleUrls: ['./posts.component.css']
+  styleUrls: ['./posts.component.css'],
 })
 export class PostsComponent implements OnInit {
+  posts!: Post[];
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
+    // to get posts from mongodb and populate 'posts' array
+    this.http.get(environment.apiEndpoint + '/Posts').subscribe((response) => {
+      this.posts = response as Post[];
+    });
   }
 
+  momentTimestamp(time: string) {
+    return moment(time).fromNow();
+  }
 }
